6d8b047027002d3a99c9468e2bcad44a4342c3be
Tomtom-dev <thomasheddadji0@gmail.com>
2021-04-09 15:31:25 +0200

    fix4

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@
 
 const fs = require('fs');
 const path = require('path');
+var pg = require('pg');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-var pg = require('pg');
 pg.defaults.ssl = true;
 
 let sequelize;
